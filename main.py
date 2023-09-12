13c01fee5632608d931e569834d89a2d0fdb7a87
Khawaja Abdul Ahad <34687971+kaa125@users.noreply.github.com>
2023-09-13 04:22:19 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,98 +1,57 @@
 #!/usr/bin/env python
 
-## imports
-from mlxtend.frequent_patterns import fpgrowth
-from mlxtend.preprocessing import TransactionEncoder
+import utility
+import transformations
+from google.cloud import bigquery
+from datetime import datetime
 import pandas as pd
 
-import utility
-import processing
-from google.cloud import bigquery
-from datetime import datetime, timedelta
-from tqdm import tqdm
-from pytz import timezone    
-
-# environment
-from dotenv import load_dotenv
-import os
-
-# loading environment variables and setting constants
-load_dotenv()
-GCP_CREDS_STRING = os.environ.get('GCP_CREDS_STRING')
-DATASET_ID = os.environ.get('DATASET_ID')
-# table to write final dataframe into in the db
-TABLE_NAME = os.environ.get('TABLE_NAME')
-DB_CONNECTION_STRING = os.environ.get('DB_CONNECTION_STRING')
-# for execution time calculation
-START_TIME = datetime.now()
-# time range for pulling records (in days) - order_items, orders etc.
-# setting to 3 months ~ 91 days
-TIME_RANGE = 91
-TODAY = datetime.today()
-# string representing extraction start date in Y-m-d format
-EXTRACTION_START_DATE = (TODAY - timedelta(days=TIME_RANGE)).strftime("%Y-%m-%d")
+# Start time for execution time calculation
+startTime = datetime.now()
 
 if __name__ == "__main__":
-    # fetch credentials
-    gcp_credentials = utility.get_gcp_credentials(GCP_CREDS_STRING)
-    utility.calculate_time("Credentials fetched", START_TIME)
-
-    # BigQuery client setup
-    bq_client = bigquery.Client(credentials=gcp_credentials, project=gcp_credentials.project_id)
-    utility.calculate_time("BigQuery client set up", START_TIME)
-
-    # Get order items data
-    print("Fetching input data...")
-    df_in = utility.load_table_from_bigquery(
-        bq_client=bq_client, 
-        dataset_id=DATASET_ID, 
-        table_name='stg_rec_sys_raw',
-        columns=['order_id','product_id'], 
-        where_clause=f"WHERE created_at > '{EXTRACTION_START_DATE}' LIMIT 800000"
-    )
-    utility.calculate_time("Raw data fetched", START_TIME)
-
-    print("Pre-processing dataframe...")
-    processed_df = processing.pre_process(df_in, count_x=4)
-    utility.calculate_time("Pre-processing done", START_TIME)
-
-    # Generate list of transaction items
-    print("Generating transaction list...")
-    transaction_list = processing.generate_items_list(processed_df)
-    utility.calculate_time("Transaction list generated", START_TIME)
-
-    # Generate one-hot encoded dataframe
-    print("One-hot encoding transactions...")
-    encoded_df = processing.encode_transactions(transaction_list)
-    utility.calculate_time("Transactions encoded", START_TIME)
-
-    # Generate result set
-    # Setting minimum support threshold for FP-Growth algorithm
-    min_support = 0.0001
-    print("Generating result set...")
-    result_set = processing.generate_result_itemsets(encoded_df, min_support=min_support)
-    utility.calculate_time("Result set generated", START_TIME)
-
-    # Post-processing result set
-    print("Post-processing result set...")
-    processed_results = processing.process_results(result_set)
-    utility.calculate_time("Result set post processed", START_TIME)
-
-    # Get unique product IDs from the itemsets only
-    list_of_itemsets = processed_results.itemsets.to_list()
-    product_id_set = set().union(*list_of_itemsets)
-    run_ids = list(product_id_set)
-    print(len(product_id_set))
-    print(len(run_ids))
     
-    print("Generating dictionary for final dataframe")
-    final_df = processing.generate_final_dataframe(processed_results, run_ids)
-    utility.calculate_time("Final dataframe generated", START_TIME)
+    # BigQuery client setup
+    print("Retrieving GCP credentials")
+    # Replace the path with your local credentials JSON file
+    gcp_credentials = utility.get_gcp_credentials_local("path/to/your/credentials.json")
+    bq_client = bigquery.Client(credentials=gcp_credentials, project=gcp_credentials.project_id)
+    utility.calculate_time("Credentials fetched", startTime)
 
-    # Write dataframe to the database
-    print("Writing dataframe to database")
-    utility.write_df_to_database(final_df, 
-            table_name=TABLE_NAME, 
-            db_connection_string=DB_CONNECTION_STRING
-    )
-    utility.calculate_time("Data written to database", START_TIME)
+    # Extract raw data - user and product IDs - from BigQuery
+    dataset_id = gcp_credentials.project_id + "." + "your_dataset_name"
+    columns_to_pull = ['user_id', 'product_id']
+    # Load raw recommender system data - user IDs and product IDs
+    print("Fetching raw user and product data")
+    rec_df_raw = utility.load_table_from_bigquery(bq_client, dataset_id, "stg_rec_sys_raw", columns_to_pull)
+    utility.calculate_time("Raw rec sys data fetched", startTime)
+
+    # Generating raw user-item interaction matrix
+    print("Generating raw user-item matrix")
+    raw_user_item_matrix = transformations.generate_user_item_matrix(rec_df_raw)
+    utility.calculate_time("Raw user-item matrix generated", startTime)
+
+    # Performing data wrangling and cleaning on the raw matrix
+    print("Processing raw user-item matrix")
+    processed_user_item_matrix = transformations.preprocess_user_item_matrix(raw_user_item_matrix)
+    utility.calculate_time("Processing on user-item matrix complete", startTime)
+
+    # Generating item-item similarity matrix
+    print("Generating item-item similarity matrix")
+    item_similarity_matrix = transformations.generate_item_similarity_matrix(processed_user_item_matrix, "jaccard")
+    utility.calculate_time("Item similarity matrix generated", startTime)
+
+    # Generating the final recommendations dataframe
+    print("Generating final recommendations dataframe")
+    final_recommendation_df = transformations.generate_final_recommendation_df(processed_user_item_matrix, item_similarity_matrix)
+    utility.calculate_time("Final recommendations dataframe generated", startTime)
+
+    final_recommendation_df = final_recommendation_df.convert_dtypes()
+    print("Writing recommendation dataframe to the database")   
+
+    # Pass the table name and database connection string as arguments
+    utility.write_df_to_database(final_recommendation_df, "your_table_name", "your_db_connection_string")
+    utility.calculate_time("Data written to database", startTime)
+
+    # To calculate the performance of the recommendation system
+    utility.get_hit_rate(final_recommendation_df, bq_client, dataset_id)
--- utility.py
+++ utility.py
@@ -1,100 +1,120 @@
-'''
-Utility functions e.g. fetching data, uploading data etc.
-'''
-
 import json
+import time
 from google.oauth2 import service_account
+from google.cloud import bigquery
 from datetime import datetime
 from sqlalchemy import create_engine
-import time
+import pandas as pd
 
-def get_gcp_credentials(credentials_string):
-    ''' Returns Google Cloud credentials from JSON string.
+def get_gcp_credentials_local(creds_fp):
+    ''' Returns Google Cloud credentials from a file path.
 
     Args:
-        credentials_string: JSON string representing GCP service account JSON
+        creds_fp: File path to GCP service account JSON file
     
     Returns:
         GCP Credentials object
     '''
     try:
-        credentials = service_account.Credentials.from_service_account_info(
-            json.loads(credentials_string))
+        credentials = service_account.Credentials.from_service_account_file(creds_fp)
         print("GCP credentials retrieved successfully")
-    except Exception as exception_message:
-        print(exception_message)
-        print("could not retrieve GCP credentials")
+    except:
+        print("Could not retrieve GCP credentials")
         credentials = None
     return credentials
 
-
 def load_table_from_bigquery(bq_client, dataset_id, table_name, columns, where_clause = ""):
-    ''' Selects columns passed as argument from the given table name
+    ''' Selects columns passed as arguments from the given table name.
 
     Args:
         bq_client: BigQuery client object
         dataset_id: ID of dataset in BigQuery
-        table_name: name of table to load from BigQuery
-        colums: fields to load from the table
-        where_clause: optional argument for filtering data
+        table_name: Name of table to load from BigQuery
+        columns: Fields to load from the table
+        where_clause: Optional argument for filtering data
     
     Returns:
-        dataframe with loaded table data
+        Dataframe with loaded table data
     '''
     query = f"""
-      SELECT {','.join(columns)}
-      FROM `dastgyr-data-warehouse.{dataset_id}.{table_name}` """ + where_clause
-    print(query)
+        SELECT {','.join(columns)}
+        FROM `{dataset_id}.{table_name}` """ + where_clause
     df = bq_client.query(query).to_dataframe()
-    print("data loaded from " + table_name)
+    print("Data loaded from " + table_name)
     return df
 
-
-def calculate_time(message, start_time):
+def calculate_time(message, startTime):
     '''A utility function which prints out the time taken since
-    a specified startTime - for checking script's execution time
-    Args:
-        message: string message to print before the time
-        start_time: the starting time of the main script
-    Returns:
-        a message with the time taken to execute till that instant/when fucntion is called
-    '''
-    print("=============================\n" + message + "\n"
-    + str(datetime.now() - start_time) + 
-    "\n=============================\n"
-    )
-
-
-
-def write_df_to_database(df_in, table_name, db_connection_string):
-    '''this functions writes the input dataframe to a table in the connected database
+    a specified startTime - for checking script's execution time.
     
     Args:
-        df_in - the dataframe to be written to the table
-        table_name - table name to write the data in
-        db_connection_string - connection string to connect to database
+        message: String message to print before the time
+        startTime: The starting time of the main script
+    
+    Returns:
+        A message with the time taken to execute till that instant/when the function is called
     '''
-    df_in['updated_at'] = datetime.now()
-    df_in['is_current'] =  1
+    print("=============================\n" + message + "\n"
+    + str(datetime.now() - startTime) + "\n=============================\n"
+    )
 
-    # establish connection with db
-    engine = create_engine(db_connection_string)
+def write_df_to_database(recommendations_df):
+    '''This function writes the input dataframe to a table in the connected database.
+    
+    Args:
+        recommendations_df: The dataframe to be written to the table
+    '''
+    recommendations_df['updated_at'] = datetime.now()
+    recommendations_df['is_current'] = True
+    # Name of table where dataframe is to be written
+    TABLE_NAME = "recommendations"
+
+    # Fetch connection string to connect to the database
+    with open('./creds/database_configuration.json') as f:
+        config = json.load(f)
+
+    # Establish connection with the database
+    engine = create_engine(config["connection_string"])
     conn = engine.connect()
 
     retries_attempted = 0
-    to_insert = True # tracks whether table has yet to be inserted
+    to_insert = True # Tracks whether table has yet to be inserted
 
     while retries_attempted < 3 and to_insert == True:
         try:
             start = time.time()
-            rows_inserted = df_in.to_sql(table_name, con=engine, if_exists='replace', index=False, chunksize = 10000)
+            rows_inserted = recommendations_df.to_sql(TABLE_NAME, con=engine, index=False, if_exists='replace', chunksize = 10000)
             end = time.time()
 
             print("write_df_to_database::Time taken for writing " + str(rows_inserted) + " records: ", end - start, "s")
             to_insert = False
 
         except Exception as e:
-            print("write_df_to_database::exception",e)
-            retries_attempted+=1
+            print("write_df_to_database::exception", e)
+            retries_attempted += 1
 
     conn.close()
+
+def get_hit_rate(recommendations_df, bq_client, dataset_id):
+    '''This function returns the hit rate %, i.e., the number of recommendations we made and how many people 
+    bought the recommended items.
+    
+    Args:
+        recommendations_df: The dataframe containing recommendations
+        bq_client: BigQuery client object
+        dataset_id: ID of dataset in BigQuery
+    '''
+    recommendation_users = recommendations_df['user_id'].nunique()
+    print("# of users recommendations are generated for: ", recommendation_users)
+
+    # Get orders of users who are both in training and test dataset
+    filter = "WHERE OrderCreatedAt >= '2022-10-10' and UserID in {}".format(tuple(recommendations_df['user_id'].unique()))
+    orders_test_dataset = load_table_from_bigquery(bq_client, dataset_id, "stg_orders_denorm", ["UserID", "PRODUCT_ID"], filter)
+    overlapping_users = orders_test_dataset["UserID"].nunique()
+    print("# of users present in both training and test datasets: ", overlapping_users)
+    print("Percentage of overlapping users: {}%".format(overlapping_users / recommendation_users * 100))
+
+    merged_df = pd.merge(orders_test_dataset, recommendations_df, how="inner", left_on=["UserID", "PRODUCT_ID"], right_on=["user_id", "product_id"])
+
+    hit_rate = len(merged_df.index) / len(orders_test_dataset.index) * 100
+    print("Hit rate: {}%".format(hit_rate))
